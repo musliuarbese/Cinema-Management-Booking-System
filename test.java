157eb673abe0de80f2d0c86014fe7d490007b021
bledardakaj <bd35191@ubt-uni.net>
2020-04-11 17:54:42 +0200

    shtova nja 10 funksione qe na kryjn pune

--- test.java
+++ test.java
@@ -1,3 +1,6 @@
 psh e kena keiju nje fajll te ri e kena shkru kod e tripa. se e kam llaptopin nprehen marova tu gabu ahahahah
 
 kur ki shkrujt mir se edhe me fol spo nin ja sdiiiiiii sdiii kurr bash
+
+
+dmth tash mu ka kriju edhe mu edhe une hi shtoj naj funksion a najsen.
